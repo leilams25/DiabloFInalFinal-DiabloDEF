43536f1e643ea72eb26f0e138c88eb0f1db5dddd
leilams25 <leilamsanz@gmail.com>
2025-02-05 02:34:41 +0100

    son las 2:34 Am xd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+    private Transform ultimoClick;
     private Camera cam;
     private NavMeshAgent agent;
   
@@ -12,27 +14,53 @@ public class Player : MonoBehaviour
     {
         cam = Camera.main;
         agent = GetComponent<NavMeshAgent>();
+
     }
 
    
     void Update()
     {
+       Movimiento();
+        ComprobarInteraccion();
+
+    }
+
+   
+
+    private void Movimiento()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
-            if(Physics.Raycast(ray , out RaycastHit hitInfo))
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
             {
 
                 agent.SetDestination(hitInfo.point);
 
+
+                ultimoClick = hitInfo.transform;
             }
 
 
         }
-      
+    }
+    private void ComprobarInteraccion()
+    {
+        if(ultimoClick != null && ultimoClick.TryGetComponent(out NPC npc))
+        { 
+            agent.stoppingDistance = 2f;
+
+            if( !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+            {
+                npc.Interactuar( this.transform);
+                ultimoClick = null;
+            }
+            else if (ultimoClick)
+            {
+               agent.stoppingDistance = 0f;
+            }
+        }
     }
 
-
-   
 }
